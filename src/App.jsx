5b62f28972a10bea31eaeccc320caaea786ba705
Manikanta <manikanta.alapati2000@gmail.com>
2022-10-09 20:09:02 -0400

    changed profile name and profile info

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ class App extends React.Component {
     super(...args);
     this.state = {
       currentSection: "news",
-      name: "Jake Smith",
+      name: "Manikanta Alapati",
       about:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "I am Naga Sai Manikanta Alapati and I am pursuing my masters in computer Science  in University At Albany and I have taken software-Engineering course for this semester.",
     };
     this.saveStateToLocalStorage = this.saveStateToLocalStorage.bind(this);
   }
